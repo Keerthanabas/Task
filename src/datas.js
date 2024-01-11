94a561f4ad56c685f4a7a51867ec8e7e6a8d63cf
Keerthana B <bkeerthanaeee@gmail.com>
2024-01-11 17:38:44 +0530

    11.1.24 changes

--- src/datas.js
+++ src/datas.js
@@ -120,7 +120,7 @@ export const data = [
       "completed": true
     },
     {
-      "userId": 1,
+      "userId": 2,
       "id": 21,
       "title": "suscipit repellat esse quibusdam voluptatem incidunt",
       "completed": false
